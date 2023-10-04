c758b0a2eb37d3aef6a128a61937ae05116415ce
davidtnfsh <davidtnfsh@gmail.com>
2023-10-04 16:29:00 +0900

    [feat](performance tab): implement this tab

---
+++ pages/views/Performance.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Card, Col, Row, Statistic } from "antd";
+const Performance = ({ portfolioApr }) => (
+  <>
+    <Row gutter={16}>
+      <Col span={12}>
+        <Card bordered={false}>
+          <Statistic
+            title="Reward APR"
+            value={portfolioApr}
+            precision={2}
+            valueStyle={{
+              color: "#3f8600",
+            }}
+            suffix="%"
+          />
+        </Card>
+      </Col>
+      <Col span={12}>
+        <Card bordered={false}>
+          <Statistic
+            title="Sharpe Ratio"
+            value="? (WIP)"
+            precision={2}
+            valueStyle={{
+              color: "#cf1322",
+            }}
+          />
+        </Card>
+      </Col>
+    </Row>
+  </>
+);
+export default Performance;
