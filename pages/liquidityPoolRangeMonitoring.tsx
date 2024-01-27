d3d4e77fbbf4b87002d681550dd56199ec88d0c3
davidtnfsh <davidtnfsh@gmail.com>
2024-01-27 22:40:47 +0700

    [feat](liquidity_pool_range): finish its POC

--- pages/liquidityPoolRangeMonitoring.tsx
+++ pages/liquidityPoolRangeMonitoring.tsx
@@ -1,14 +1,30 @@
 import type { NextPage } from "next";
 import BasePage from "./basePage.tsx";
-import { Badge, Card } from "antd";
-import { useContractRead } from "wagmi";
-import { useEffect } from "react";
+import { Badge, Card, Image, Spin } from "antd";
+import { useEffect, useState } from "react";
 import { useWindowHeight } from "../utils/chartUtils";
-import camelotNFTPositionManager from "../lib/contracts/CamelotNFTPositionManager.json";
 
-interface PositionObj {
-  data: any;
+interface Pool {
+  data: {
+    pool: { chain: string; adapter_id: string };
+    asset_token_list: { symbol: string }[];
+  };
+  current_price: number;
+  in_range: boolean;
+  max_price: number;
+  min_price: number;
 }
+
+const DebankChainToDefillamaMapping: Record<string, string> = {
+  arb: "arbitrum",
+  op: "optimism",
+  eth: "ethereum",
+  bsc: "bsc",
+};
+const DebankProjectIdToDefillamaMapping: Record<string, string> = {
+  uniswap3_liquidity: "uniswap-v3",
+  camelot_v31_liquidity: "camelot-v3",
+};
 
 const LiquidityPoolRangeMonitoring: NextPage = () => {
   const windowHeight = useWindowHeight();
@@ -17,37 +33,68 @@ const LiquidityPoolRangeMonitoring: NextPage = () => {
     minHeight: windowHeight,
     color: "#ffffff",
   };
-  const BPS = 1.0001;
-  const camelotNftPositionManager =
-    "0x00c7f3082833e796a5b3e4bd59f6642ff44dcd15";
-  const camelotPositionPendle1: PositionObj = useContractRead({
-    address: camelotNftPositionManager,
-    abi: camelotNFTPositionManager,
-    functionName: "positions",
-    args: [47230],
-    chainId: 42161,
+  const [statusesOfLiquidityPools, setstatusesOfLiquidityPools] = useState({
+    data: [],
   });
-  const camelotPositionPendle2: PositionObj = useContractRead({
-    address: camelotNftPositionManager,
-    abi: camelotNFTPositionManager,
-    functionName: "positions",
-    args: [47232],
-    chainId: 42161,
-  });
-  const camelotPositionLink: PositionObj = useContractRead({
-    address: camelotNftPositionManager,
-    abi: camelotNFTPositionManager,
-    functionName: "positions",
-    args: [39409],
-    chainId: 42161,
-  });
-  const NFTPositions = {
-    camelotPositionPendle1,
-    camelotPositionPendle2,
-    camelotPositionLink,
+
+  const renderCardTitle = (pool: Pool) => {
+    return (
+      <>
+        <Image
+          src={`/chainPicturesWebp/${
+            DebankChainToDefillamaMapping[pool.data.pool.chain] ??
+            pool.data.pool.chain
+          }.webp`}
+          height={20}
+          width={20}
+        />{" "}
+        -
+        <Image
+          src={`/projectPictures/${
+            DebankProjectIdToDefillamaMapping[pool.data.pool.adapter_id] ??
+            pool.data.pool.adapter_id
+          }.webp`}
+          height={20}
+          width={20}
+        />{" "}
+        -
+        {pool.data.asset_token_list.map((symbolObj) => (
+          <Image
+            key={symbolObj.symbol}
+            src={`/tokenPictures/${symbolObj.symbol.toLowerCase()}.webp`}
+            alt={symbolObj.symbol}
+            height={20}
+            width={20}
+          />
+        ))}
+      </>
+    );
   };
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const userApiKey = "placeholder";
+    async function getPositions() {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/liquidity_pool_ranges`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            user_api_key: userApiKey,
+          }),
+        },
+      );
+      const statuses_of_liquidity_pools = await response.json();
+      console.log(
+        "statuses_of_liquidity_pools",
+        statuses_of_liquidity_pools.data,
+      );
+      setstatusesOfLiquidityPools(statuses_of_liquidity_pools);
+    }
+    getPositions();
+  }, []);
 
   return (
     <BasePage>
@@ -55,22 +102,36 @@ const LiquidityPoolRangeMonitoring: NextPage = () => {
         <center>
           <h1>Liquidity Pool Range Monitoring Service</h1>
         </center>
-        {Object.entries(NFTPositions).map(([name, positionObj], index) => {
-          return (
-            <Badge.Ribbon text="In Range" color="green">
-              <Card title={name} size="small">
-                {positionObj.data === undefined ? (
-                  <p>loading...</p>
-                ) : (
+
+        {statusesOfLiquidityPools.data.length === 0 ? (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "15rem",
+            }}
+          >
+            <Spin size="large" />
+          </div>
+        ) : (
+          statusesOfLiquidityPools.data.map((pool: Pool, index) => {
+            return (
+              <Badge.Ribbon
+                text={pool.in_range === true ? "In Range" : "Out of Range"}
+                color={pool.in_range === true ? "green" : "red"}
+              >
+                <Card title={renderCardTitle(pool)} size="small">
                   <p>
-                    Price Range: {Math.pow(BPS, positionObj.data[4]).toFixed(5)}
-                    -{Math.pow(BPS, positionObj.data[5]).toFixed(5)}
+                    Price Range: {pool.min_price.toFixed(5)}-
+                    {pool.max_price.toFixed(5)}, Current Price:{" "}
+                    {pool.current_price.toFixed(5)}
                   </p>
-                )}
-              </Card>
-            </Badge.Ribbon>
-          );
-        })}
+                </Card>
+              </Badge.Ribbon>
+            );
+          })
+        )}
       </div>
     </BasePage>
   );
