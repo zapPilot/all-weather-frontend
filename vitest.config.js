1c23a6b1d2aa48bb5db7d09521535f3465f721b5
Fanlin <fanchi0917@gmail.com>
2024-01-29 13:40:52 +0800

    [feat](vitest): add vitest yaml and template (#95)
    * [feat](vitest): add vitest yaml and template
    
    * [feat](vitest): remove unit test ignore
    
    * [chores/format]: prettier
    
    * [chores/format]: prettier

--- vitest.config.js
+++ vitest.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID =
+  "bee6af70ea57e6499462532060febf40";
 
 export default defineConfig({
   plugins: [react()],
