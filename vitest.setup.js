315d0edc8fb4eb62caa2bef06a25f3971e0d1b83
Fanlin <fanchi0917@gmail.com>
2024-02-14 11:34:43 +0800

    [fix](vitest): add Custom Render for vitest (#122)
    * [fix](vitest): add Custom Render for vitest
    
    * [chores/format]: prettier
    
    * [fix](vitest): update rainbowkit package
    
    * [fix](vitest): change "@walletconnect/modal" version

---
+++ vitest.setup.js
@@ -0,0 +1,13 @@
+import "@testing-library/jest-dom/vitest";
+
+if (typeof window !== "undefined") {
+  window.matchMedia =
+    window.matchMedia ||
+    function () {
+      return {
+        matches: false,
+        addListener: function () {},
+        removeListener: function () {},
+      };
+    };
+}
