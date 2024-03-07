23c3cc56935a7554677bd725c947aef04f48550e
davidtnfsh <davidtnfsh@gmail.com>
2024-03-07 15:24:57 +0700

    [wip]: use particle as an example

--- utils/etherspot.js
+++ utils/etherspot.js
@@ -21,7 +21,7 @@ import { SmartAccount } from "@particle-network/aa";
 // add/change these values
 const precisionOfInvestAmount = 4;
 // const approvalBufferParam = 1.2;
-const approvalBufferParam = 100;
+const approvalBufferParam = 100000000;
 
 //  `Error: execution reverted: STF` means there's no enough tokens to safe transfer from
 const slippage = [0.1, 0.5, 1, 10, 50];
@@ -36,6 +36,7 @@ const wethAddress = "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1";
 const linkAddress = "0xf97f4df75117a78c1A5a0DBb814Af92458539FB4";
 const axlAddress = "0x23ee2343B892b1BB63503a4FAbc840E0e2C6810f";
 const usdcAddress = "0xaf88d065e77c8cC2239327C5EDb3A432268e5831";
+const usdtAddress = "0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9";
 const gmxAddress = "0xfc5A1A6EB076a2C7aD06eD22C90d7E710E35ad0a";
 const wsolAddress = "0x2bcC6D6CdBbDC0a4071e48bb3B969b06B3330c07";
 const rdntAddress = "0x3082CC23568eA640225c2467653dB90e9250AaA0";
@@ -50,41 +51,16 @@ export async function investByAAWallet(
   walletClient,
   chain,
 ) {
-  const smartAccount = new SmartAccount(walletClient, {
-    projectId: "b46bf180-cc72-4691-8530-8366006d7468",
-    clientKey: "c1S1QbbckW5nwUV9EQFszan25oJCFwfVr7LPTqDd",
-    appId: "particle app id",
-    aaOptions: {
-      accountContracts: {
-        // 'BICONOMY', 'CYBERCONNECT', 'SIMPLE' is supported now.
-        SIMPLE: [
-          {
-            version: "1.0.0",
-            chainIds: [chain.id],
-          },
-        ],
-      },
-      // paymasterApiKeys: [{
-      //     chainId: chain.id,
-      //     apiKey: 'paymaster api key',
-      // }]
-    },
-  });
-  console.log(
-    "smartAccount",
-    await smartAccount.getAddress(),
-    await smartAccount.getOwner(),
-    await smartAccount.getAccount(),
-  );
   console.log("Investing by AA Wallet...");
   const portfolioHelper = await getPortfolioHelper(
     "AllWeatherPortfolio",
     walletClient,
+    chain
   );
-  // const transactionHash = await portfolioHelper.diversify(
-  //   investmentAmount,
-  //   chosenToken,
-  // );
+  const transactionHash = await portfolioHelper.diversify(
+    investmentAmount,
+    chosenToken,
+  );
   // console.log("transactionHash", transactionHash);
   // const dataService = new DataUtils(
   //     "public-prime-testnet-key",
@@ -97,39 +73,54 @@ export async function investByAAWallet(
   // console.log("\x1b[33m%s\x1b[0m", `EtherspotWallet balances:`, balances);
 }
 
-async function getPortfolioHelper(portfolioName, provider) {
-  console.log("provider", provider);
+async function getPortfolioHelper(portfolioName, walletClient, chain) {
   let portfolioHelper;
   if (portfolioName === "AllWeatherPortfolio") {
-    portfolioHelper = new AllWeatherPortfolio(provider);
+    portfolioHelper = new AllWeatherPortfolio(walletClient, chain);
   }
   await portfolioHelper.initialize();
   return portfolioHelper;
 }
 
 export class AllWeatherPortfolio {
-  constructor(provider) {
+  constructor(walletClient, chain) {
     // initializating sdk...
-    const customBundlerUrl = "";
-    this.primeSdk = new PrimeSdk(
-      provider,
-      // { privateKey: process.env.NEXT_PUBLIC_WALLET_PRIVATE_KEY },
-      {
-        chainId: Number(process.env.NEXT_PUBLIC_CHAIN_ID),
-        projectKey: "all-weather-dev",
-        bundlerProvider: new EtherspotBundler(
-          Number(process.env.NEXT_PUBLIC_CHAIN_ID),
-          process.env.NEXT_PUBLIC_ETHERSPOT_PROJECT_KEY,
-          customBundlerUrl,
-        ),
+    this.AAWallet = new SmartAccount(walletClient, {
+      projectId: "b46bf180-cc72-4691-8530-8366006d7468",
+      clientKey: "c1S1QbbckW5nwUV9EQFszan25oJCFwfVr7LPTqDd",
+      appId: "particle app id",
+      aaOptions: {
+        accountContracts: {
+          SIMPLE: [
+            {
+              version: "1.0.0",
+              chainIds: [chain.id],
+            },
+          ],
+        },
+        // paymasterApiKeys: [{
+        //     chainId: chain.id,
+        //     apiKey: 'paymaster api key',
+        // }]
       },
-    ); // Testnets dont need apiKey on bundlerProvider
-    this.EOAAddress = this.primeSdk.state.EOAAddress;
+    });
+
+    this.EOAAddress = walletClient.account.address;
+    this.txs = {
+      long_term_bond: [],
+      intermediate_term_bond: [],
+      commodities: [],
+      gold: [],
+      large_cap_us_stocks: [],
+      small_cap_us_stocks: [],
+      non_us_developed_market_stocks: [],
+      non_us_emerging_market_stocks: [],
+    }
   }
   async initialize() {
     // get address of EtherspotWallet...
-    this.aaWalletAddress = await this.primeSdk.getCounterFactualAddress();
-    console.log("aaWalletAddress", this.aaWalletAddress);
+    this.aaWalletAddress = await this.AAWallet.getAddress();
+    console.log("aaWalletAddress", this.aaWalletAddress)
     this.strategy = {
       long_term_bond: {
         42161: [
@@ -138,98 +129,98 @@ export class AllWeatherPortfolio {
               42161,
               wstEthAddress,
               wethAddress,
-              this.primeSdk,
+              this.txs,
               this.aaWalletAddress,
             ),
             weight: 0.13,
           },
         ],
       },
-      intermediate_term_bond: {
-        42161: [
-          {
-            interface: new CamelotV3(
-              42161,
-              pendleAddress,
-              wethAddress,
-              this.primeSdk,
-              this.aaWalletAddress,
-            ),
-            weight: 0.15 * 2,
-          },
-        ],
-      },
-      commodities: {},
-      gold: {
-        42161: [
-          {
-            interface: new CamelotV3(
-              42161,
-              wethAddress,
-              gmxAddress,
-              this.primeSdk,
-              this.aaWalletAddress,
-            ),
-            weight: 0.075 * 2,
-          },
-        ],
-      },
-      large_cap_us_stocks: {
-        42161: [
-          {
-            interface: new CamelotV3(
-              42161,
-              wethAddress,
-              linkAddress,
-              this.primeSdk,
-              this.aaWalletAddress,
-            ),
-            weight: 0.09 * 2,
-          },
-        ],
-      },
-      small_cap_us_stocks: {
-        42161: [
-          {
-            interface: new CamelotV3(
-              42161,
-              rdntAddress,
-              wethAddress,
-              this.primeSdk,
-              this.aaWalletAddress,
-            ),
-            weight: 0.03 * 2,
-          },
-        ],
-      },
-      non_us_developed_market_stocks: {
-        42161: [
-          {
-            interface: new CamelotV3(
-              42161,
-              wsolAddress,
-              usdcAddress,
-              this.primeSdk,
-              this.aaWalletAddress,
-            ),
-            weight: 0.06 * 2,
-          },
-        ],
-      },
-      non_us_emerging_market_stocks: {
-        42161: [
-          {
-            interface: new CamelotV3(
-              42161,
-              magicAddress,
-              wethAddress,
-              this.primeSdk,
-              this.aaWalletAddress,
-            ),
-            weight: 0.03 * 2,
-          },
-        ],
-      },
+      // intermediate_term_bond: {
+      //   42161: [
+      //     {
+      //       interface: new CamelotV3(
+      //         42161,
+      //         pendleAddress,
+      //         wethAddress,
+      //         this.txs,
+      //         this.aaWalletAddress,
+      //       ),
+      //       weight: 0.15 * 2,
+      //     },
+      //   ],
+      // },
+      // commodities: {},
+      // gold: {
+      //   42161: [
+      //     {
+      //       interface: new CamelotV3(
+      //         42161,
+      //         wethAddress,
+      //         gmxAddress,
+      //         this.txs,
+      //         this.aaWalletAddress,
+      //       ),
+      //       weight: 0.075 * 2,
+      //     },
+      //   ],
+      // },
+      // large_cap_us_stocks: {
+      //   42161: [
+      //     {
+      //       interface: new CamelotV3(
+      //         42161,
+      //         wethAddress,
+      //         linkAddress,
+      //         this.txs,
+      //         this.aaWalletAddress,
+      //       ),
+      //       weight: 0.09 * 2,
+      //     },
+      //   ],
+      // },
+      // small_cap_us_stocks: {
+      //   42161: [
+      //     {
+      //       interface: new CamelotV3(
+      //         42161,
+      //         rdntAddress,
+      //         wethAddress,
+      //         this.txs,
+      //         this.aaWalletAddress,
+      //       ),
+      //       weight: 0.03 * 2,
+      //     },
+      //   ],
+      // },
+      // non_us_developed_market_stocks: {
+      //   42161: [
+      //     {
+      //       interface: new CamelotV3(
+      //         42161,
+      //         wsolAddress,
+      //         usdcAddress,
+      //         this.txs,
+      //         this.aaWalletAddress,
+      //       ),
+      //       weight: 0.06 * 2,
+      //     },
+      //   ],
+      // },
+      // non_us_emerging_market_stocks: {
+      //   42161: [
+      //     {
+      //       interface: new CamelotV3(
+      //         42161,
+      //         magicAddress,
+      //         wethAddress,
+      //         this.txs,
+      //         this.aaWalletAddress,
+      //       ),
+      //       weight: 0.03 * 2,
+      //     },
+      //   ],
+      // },
     };
     this._checkTotalWeight(this.strategy);
   }
@@ -245,9 +236,9 @@ export class AllWeatherPortfolio {
       }
     }
     console.log("Total Weight: ", totalWeight);
-    if (Math.abs(totalWeight - 1) > 0.0001) {
-      throw new Error("Total weight of all protocols must be 1");
-    }
+    // if (Math.abs(totalWeight - 1) > 0.0001) {
+    //   throw new Error("Total weight of all protocols must be 1");
+    // }
   }
   async diversify(investmentAmount, chosenToken) {
     const transactionHashes = await this._diversify(
@@ -262,18 +253,24 @@ export class AllWeatherPortfolio {
     for (const [category, protocolsInThisCategory] of Object.entries(
       this.strategy,
     )) {
+      this._clearTxs(category);
       for (const [chainId, protocols] of Object.entries(
         protocolsInThisCategory,
       )) {
         const transactionHash = await this._retryFunction(
           this._investInThisCategory.bind(this),
           { investmentAmount, chosenToken, protocols, category },
-          { retries: 5, delay: 1000 },
+          // { retries: 5, delay: 1000 },
+          { retries: 1, delay: 1000 },
         );
         transactionHashes.push(transactionHash);
       }
     }
     return transactionHashes;
+  }
+
+  _clearTxs(category) {
+    this.txs[category] = [];
   }
   async _investInThisCategory({
     investmentAmount,
@@ -282,14 +279,13 @@ export class AllWeatherPortfolio {
     category,
     retryIndex,
   }) {
-    // clear the transaction batch
-    await this.primeSdk.clearUserOpsFromBatch();
     let concurrentRequests = [];
     for (const protocol of protocols) {
       const investPromise = protocol.interface.invest(
         (investmentAmount * protocol.weight).toFixed(precisionOfInvestAmount),
         chosenToken,
         retryIndex,
+        category
       );
       concurrentRequests.push(investPromise);
     }
@@ -299,11 +295,12 @@ export class AllWeatherPortfolio {
 
   async _signTransaction(category) {
     // estimate transactions added to the batch and get the fee data for the UserOp
-    const op = await this.primeSdk.estimate();
-    console.log(`Investment in ${category} completed...`);
+    console.log(`${category} txs`, this.txs)
+    const feeQuotesResult = await this.AAWallet.getFeeQuotes(this.txs[category]);
+    console.log(`Investment in ${category} completed...`, feeQuotesResult);
     // console.log(`Estimate UserOp: ${await printOp(op)}`);
     //   // sign the UserOp and sending to the bundler...
-    //   const uoHash = await primeSdk.send(op);
+    //   const uoHash = await AAWallet.send(op);
     //   console.log(`UserOpHash: ${uoHash}`);
     //   // get transaction hash...
     //   console.log("Waiting for transaction...");
@@ -311,7 +308,7 @@ export class AllWeatherPortfolio {
     //   const timeout = Date.now() + 60000; // 1 minute timeout
     //   while (userOpsReceipt == null && Date.now() < timeout) {
     //     await sleep(2);
-    //     userOpsReceipt = await primeSdk.getUserOpReceipt(uoHash);
+    //     userOpsReceipt = await AAWallet.getUserOpReceipt(uoHash);
     //   }
     // return uoHash;
   }
@@ -336,18 +333,18 @@ export class AllWeatherPortfolio {
 }
 
 class CamelotV3 {
-  constructor(chaindId, token0, token1, primeSdk, aaWalletAddress) {
+  constructor(chaindId, token0, token1, txs, aaWalletAddress) {
     this.chainId = chaindId;
     this.token0 = token0;
     this.token1 = token1;
-    this.primeSdk = primeSdk;
+    this.txs = txs;
     this.provider = new ethers.providers.JsonRpcProvider(
       process.env.NEXT_PUBLIC_BUNDLER_URL,
     );
     this.aaWalletAddress = aaWalletAddress;
   }
 
-  async invest(investmentAmountInThisPosition, chosenToken, retryIndex) {
+  async invest(investmentAmountInThisPosition, chosenToken, retryIndex, category) {
     // get erc20 Contract Interface
     const erc20Instance = new ethers.Contract(
       chosenToken,
@@ -357,33 +354,51 @@ class CamelotV3 {
 
     // get decimals from erc20 contract
     const decimals = (await erc20Instance.functions.decimals())[0];
-    await this.primeSdk.addUserOpsToBatch({
-      to: chosenToken,
+    this.txs[category].push({
+      to: usdtAddress,
       data: encodeFunctionData({
         abi: permanentPortfolioJson.abi,
         functionName: "approve",
         args: [
           oneInchAddress,
           ethers.utils.parseUnits(
-            String(
-              (investmentAmountInThisPosition * approvalBufferParam).toFixed(
-                decimals,
-              ),
-            ),
-            decimals,
-            // 6
+            // String(
+            //   (investmentAmountInThisPosition * approvalBufferParam).toFixed(
+            //     decimals,
+            //   ),
+            // ),
+            // decimals,
+            "10000000",
+            6
           ),
         ],
       }),
     });
-    const [token0Amount, token1Amount] = await this._concurrentSwap(
-      chosenToken,
-      investmentAmountInThisPosition,
-      decimals,
-      retryIndex,
+    const swapCallDataFrom1inch = await fetch1InchSwapData(
+      42161,
+      usdtAddress,
+      pendleAddress,
+      ethers.utils.parseUnits(
+        "1",
+        6
+      ),
+      this.aaWalletAddress,
+      50,
     );
-    await this._concurrentApprove(token0Amount, token1Amount);
-    await this._deposit(token0Amount, token1Amount, retryIndex);
+    this.txs[category].push({
+      to: oneInchAddress,
+      data: swapCallDataFrom1inch["tx"]["data"],
+    });
+
+    // const [token0Amount, token1Amount] = await this._concurrentSwap(
+    //   chosenToken,
+    //   investmentAmountInThisPosition,
+    //   decimals,
+    //   retryIndex,
+    //   category
+    // );
+    // await this._concurrentApprove(token0Amount, token1Amount, category);
+    // await this._deposit(token0Amount, token1Amount, retryIndex, category);
   }
 
   async _concurrentSwap(
@@ -391,33 +406,45 @@ class CamelotV3 {
     investmentAmountInThisPosition,
     decimals,
     retryIndex,
+    category
   ) {
-    let tokenSwapPromises = [];
-    for (const token of [this.token0, this.token1]) {
-      tokenSwapPromises.push(
-        this._swap(
-          chosenToken,
-          token,
+    // let tokenSwapPromises = [];
+    // for (const token of [this.token0, this.token1]) {
+      // tokenSwapPromises.push(
+      console.log(          ethers.utils.parseUnits(
+        String(investmentAmountInThisPosition / 2),
+        decimals,
+      ), "Amount")
+        await this._swap(
+          usdtAddress,
+          pendleAddress,
+          // ethers.utils.parseUnits(
+          //   String(investmentAmountInThisPosition / 2),
+          //   decimals,
+          // ),
           ethers.utils.parseUnits(
-            String(investmentAmountInThisPosition / 2),
-            decimals,
+            String(0.0001),
+            6,
           ),
-          slippage[retryIndex],
-        ),
-      );
-    }
-    const [token0Amount, token1Amount] = await Promise.all(tokenSwapPromises);
+          50,
+          // slippage[retryIndex],
+          category
+        )
+      // );
+    // }
+    return [1, 2]
+    // const [token0Amount, token1Amount] = await Promise.all(tokenSwapPromises);
     return [token0Amount, token1Amount];
   }
 
-  async _concurrentApprove(token0Amount, token1Amount) {
+  async _concurrentApprove(token0Amount, token1Amount, category) {
     let tokenApprovePromises = [];
     for (const [token, tokenAmount] of [
       [this.token0, token0Amount],
       [this.token1, token1Amount],
     ]) {
       tokenApprovePromises.push(
-        this._approve(token, CamelotNFTPositionManagerAddress, tokenAmount),
+        this._approve(token, CamelotNFTPositionManagerAddress, tokenAmount, category),
       );
     }
     await Promise.all(tokenApprovePromises);
@@ -428,7 +455,8 @@ class CamelotV3 {
   async rebalance() {
     throw new Error("This function is not implemented yet.");
   }
-  async _swap(fromTokenAddress, toTokenAddress, amount, slippage) {
+  async _swap(fromTokenAddress, toTokenAddress, amount, slippage, category) {
+    console.log("chain id", this.chainId, this.aaWalletAddress)
     const swapCallDataFrom1inch = await fetch1InchSwapData(
       this.chainId,
       fromTokenAddress,
@@ -437,14 +465,14 @@ class CamelotV3 {
       this.aaWalletAddress,
       slippage,
     );
-    await this.primeSdk.addUserOpsToBatch({
+    await this.txs[category].push({
       to: oneInchAddress,
       data: swapCallDataFrom1inch["tx"]["data"],
     });
     return swapCallDataFrom1inch["toAmount"];
   }
-  async _approve(tokenAddress, spenderAddress, amount) {
-    await this.primeSdk.addUserOpsToBatch({
+  async _approve(tokenAddress, spenderAddress, amount, category) {
+    await this.txs[category].push({
       to: tokenAddress,
       data: encodeFunctionData({
         abi: permanentPortfolioJson.abi,
@@ -456,7 +484,7 @@ class CamelotV3 {
       }),
     });
   }
-  async _deposit(token0Amount, token1Amount, retryIndex) {
+  async _deposit(token0Amount, token1Amount, retryIndex, category) {
     const camelotCallData = encodeFunctionData({
       abi: CamelotNFTPositionManager,
       functionName: "mint",
@@ -476,7 +504,7 @@ class CamelotV3 {
         },
       ],
     });
-    await this.primeSdk.addUserOpsToBatch({
+    await this.txs[category].push({
       to: CamelotNFTPositionManagerAddress,
       data: camelotCallData,
     });
