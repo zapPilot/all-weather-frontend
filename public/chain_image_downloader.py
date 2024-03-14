1c1acc41870505df56d906c2010acab2bba333d7
davidtnfsh <davidtnfsh@gmail.com>
2024-03-15 00:08:02 +0900

    [fix]

--- public/chain_image_downloader.py
+++ public/chain_image_downloader.py
@@ -18,9 +18,13 @@ chainsUnique = {
 for idx, chain in enumerate(chainsUnique):
     chain = chain.lower()
     print("progress:", idx / len(chainsUnique) * 100, "%")
+    filepath = f"./chainPicturesWebp/{chain}.webp"
+    if os.path.exists(filepath):
+        print(f"Skipping {chain} as it already exists")
+        continue
     img_data = requests.get(
         f"https://icons.llamao.fi/icons/chains/rsz_{chain}?w=48&h=48"
     ).content
-    img_name = os.path.join("./", f"./chainPicturesWebp/{chain}.webp")
+    img_name = os.path.join("./", filepath)
     with open(img_name, "wb") as file:
         file.write(img_data)
