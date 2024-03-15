3653babda309777694fb55ed521e6b3159b79f9b
davidtnfsh <davidtnfsh@gmail.com>
2024-03-15 11:03:31 +0900

    [test]: debug on staging

--- classes/AllWeatherPortfolio.js
+++ classes/AllWeatherPortfolio.js
@@ -152,6 +152,10 @@ export class AllWeatherPortfolio extends React.Component {
   }
   async initialize() {
     // get address of EtherspotWallet...
+    console.log(
+      "initialize: ",
+      `${process.env.NEXT_PUBLIC_API_URL}/pools/${this.concatenatedString}`,
+    );
     await fetch(
       `${process.env.NEXT_PUBLIC_API_URL}/pools/${this.concatenatedString}`,
     )
@@ -161,7 +165,7 @@ export class AllWeatherPortfolio extends React.Component {
       })
       .catch((error) => this.setState({ error }));
 
-    this.aaWalletAddress = await this.primeSdk.getCounterFactualAddress();
+    // this.aaWalletAddress = await this.primeSdk.getCounterFactualAddress();
     this._checkTotalWeight(AllWeatherPortfolio.strategy);
   }
   componentDidMount() {}
