364d0f0f3796a54194d1b3141407a07f59265d0f
Chris.Fan <fanchi0917@gmail.com>
2024-04-19 18:32:28 +0800

    [feat](vitest): add dashboard fetch protocol link test

--- __tests__/dashboard.test.jsx
+++ __tests__/dashboard.test.jsx
@@ -23,45 +23,89 @@ vi.mock("next/navigation", async () => {
   };
 });
 
-vi.mock('node-fetch', () => {
-  return {
-    default: vi.fn(() => Promise.resolve({
-      json: () => Promise.resolve({
-        data: [{
-          chain: "avalanche",
-          pool: {
-            meta: null,
-            name: "struct-finance",
-            poolID: "87e6c43a-bc0f-4a4f-adcd-1f5bff02402b",
-          },
-          symbol: 'btc.b',
-          tvlUsd: "538161",
-          apr: {
-            predictions: {
-              binnedConfidence: 2,
-              predictedClass: "Down",
-              predictedProbability: 65,
-            },
-            value: 9.53
-          }
-        }],
-        unique_query_tokens: ["btc"],
-        unexpandable: true,
-      })
-    }))
-  }
-});
-
-describe('Dashboard Component', () => {
-  it('displays multiple instances of data fetched from the API', async () => {
+describe("Dashboard Component", () => {
+  it("displays multiple instances of data fetched from the API", async () => {
+    // Mock the fetch API
+    vi.mock('node-fetch', () => {
+      return {
+        default: vi.fn(() => Promise.resolve({
+          json: () => Promise.resolve({
+            data: [{
+              chain: "avalanche",
+              pool: {
+                meta: null,
+                name: "struct-finance",
+                poolID: "87e6c43a-bc0f-4a4f-adcd-1f5bff02402b",
+              },
+              symbol: "btc.b",
+              tvlUsd: "538161",
+              apr: {
+                predictions: {
+                  binnedConfidence: 2,
+                  predictedClass: "Down",
+                  predictedProbability: 65,
+                },
+                value: 9.53
+              }
+            }],
+            unique_query_tokens: ["btc"],
+            unexpandable: true,
+          })
+        }))
+      }
+    });
+    // Render the Dashboard component
     render(<Dashboard />);
 
     await waitFor(() => {
       try {
+        // Check if the component renders the data
         const btcElements = screen.getAllByText(/.*btc.*/i);
         expect(btcElements.length).toBeGreaterThan(0);
 
         btcElements.forEach(element => {
+          expect(element).toBeInTheDocument();
+        });
+      } catch (error) {
+        console.error("An error occurred:", error);
+      }
+    });
+  });
+
+  it("fetches protocol link data", async () => {
+    // Mock the fetch API
+    vi.mock("node-fetch", () => {
+      return {
+        default: vi.fn(() =>
+          Promise.resolve({
+            json: () =>
+              Promise.resolve({
+                data: [
+                  {
+                    "name": "Binance CEX",
+                    "url": "https://www.binance.com",
+                    "slug": "binance-cex"
+                  },
+                  {
+                    "name": "Lido",
+                    "url": "https://lido.fi/",
+                    "slug": "lido"
+                  },
+                ]
+              }),
+          })
+        ),
+      };
+    });
+
+    // Render the Dashboard component
+    render(<Dashboard />);
+    await waitFor(() => {
+      try {
+        // Check if the component renders the data
+        const linkButtons = screen.getByRole('button', { name: "export" });
+        expect(linkButtons.length).toBeGreaterThan(0);
+        linkButtons.forEach((element) => {
           expect(element).toBeInTheDocument();
         });
       } catch (error) {
