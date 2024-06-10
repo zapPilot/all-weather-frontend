8ed55384d96d82590763a2aa9cf9660aea296869
Chris.Fan <fanchi0917@gmail.com>
2024-06-11 01:26:43 +0800

    [refactor](APRPopOver): Add icon and missing pool new layout

--- pages/views/APRPopOver.jsx
+++ pages/views/APRPopOver.jsx
@@ -1,7 +1,8 @@
 //@ts-nocheck
 // All code in this file will be ignored by the TypeScript compiler
 import React, { useState, useEffect } from "react";
-import { Popover, Spin } from "antd";
+import { Spin } from "antd";
+import { WarningOutlined } from "@ant-design/icons";
 import { useSelector } from "react-redux";
 
 const APRPopOver = () => {
@@ -33,39 +34,50 @@ const APRPopOver = () => {
 
   return (
     <>
-      <p>Missing Pools</p>
-      {missingPools.map((pool) => {
-        const poolJson = JSON.parse(pool.apr.debug);
-        return (
-          <>
-            <div className="flex">
-              <img
-                src={`/chainPicturesWebp/${poolJson.mapping_chain}.webp`}
-                width={20}
-                height={20}
-                alt={poolJson.mapping_chain}
-              />
-              <span>{poolJson.mapping_chain.toUpperCase()}</span>
-              <span className="mx-2">-</span>
-              <img src={pool.protocol_logo_url} width={20} height={20} alt={poolJson.project_id} />
-              <span>{poolJson.project_id}</span>
-            </div>
-            <div>
-              <span>{poolJson.sorted_debank_optimized_symbol_list?.join("-").toUpperCase()}</span>
-            </div>
-            <div className="flex justify-between">
-              <span>Your pool value</span>
-              <span>${pool.worth.toFixed(2)}</span>
-            </div>
-            <div className="flex justify-between">
-              <span>Pool APR</span>
-              <span>{pool.apr.value * 100}%</span>
-            </div>
-            <hr />
-          </>
-        )
-      })}
-      <Popover
+      <p
+        className="text-xl font-medium mb-2 cursor-pointer"
+        onClick={() => setOpen(!open)}
+      >
+        Missing Pools 
+        <WarningOutlined className="text-red-400 ms-2"/>
+      </p>
+      {open && (
+        <div className="grid grid-cols-1 gap-4">
+          <p>There might be some missing pools. Please allow at least 24 hours for customer support to update the pool data.</p>
+          {missingPools.map((pool) => {
+            const poolJson = JSON.parse(pool.apr.debug);
+            return (
+              <div className="bg-white/20 p-2">
+                <div className="flex mb-1">
+                  <img
+                    src={`/chainPicturesWebp/${poolJson.mapping_chain}.webp`}
+                    width={20}
+                    height={20}
+                    alt={poolJson.mapping_chain}
+                  />
+                  <span className="mx-1">{poolJson.mapping_chain.toUpperCase()}</span>
+                  <span>-</span>
+                  <img src={pool.protocol_logo_url} width={20} height={20} alt={poolJson.project_id} className="mx-1"/>
+                  <span>{poolJson.project_id}</span>
+                </div>
+                <div className="mb-1">
+                  <span>{poolJson.sorted_debank_optimized_symbol_list?.join("-").toUpperCase()}</span>
+                </div>
+                <div className="flex justify-between mb-1">
+                  <span>Your pool value</span>
+                  <span>${pool.worth.toFixed(2)}</span>
+                </div>
+                <div className="flex justify-between">
+                  <span>Pool APR</span>
+                  <span className="text-green-700">{pool.apr.value * 100}%</span>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      )}
+      
+      {/* <Popover
         content={missingPools.map((pool) => {
           const poolJson = JSON.parse(pool.apr.debug);
           return (
@@ -124,7 +136,7 @@ const APRPopOver = () => {
             </span>
           )}
         </a>
-      </Popover>
+      </Popover> */}
     </>
   );
 };
