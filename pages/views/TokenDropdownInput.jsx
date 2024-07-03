cea86fd3c1ec99550145abf74017b1d2814f1f66
davidtnfsh <davidtnfsh@gmail.com>
2024-07-03 15:18:19 +0900

    [feat](routePreview): fix the selected token and input amount

--- pages/views/TokenDropdownInput.jsx
+++ pages/views/TokenDropdownInput.jsx
@@ -1,78 +1,78 @@
-import { Button, Space } from "antd";
-import { useState } from "react";
-import { selectBefore } from "../../utils/contractInteractions";
-import NumericInput from "./NumberInput";
+import React, { useState, useCallback, useEffect, memo } from "react";
+import { Button, Space, Select } from "antd";
 import { useReadContract } from "thirdweb/react";
 import { balanceOf } from "thirdweb/extensions/erc20";
 import { getContract } from "thirdweb";
 import THIRDWEB_CLIENT from "../../utils/thirdweb";
 import { useActiveWalletChain } from "thirdweb/react";
 import { arbitrum } from "thirdweb/chains";
+import NumericInput from "./NumberInput";
+import { selectBefore } from "../../utils/contractInteractions";
 
-const TokenDropdownInput = () => {
-  const [chosenToken, setChosenToken] = useState("");
-  const chainId = useActiveWalletChain();
-  const contract = getContract({
-    client: THIRDWEB_CLIENT,
-    chain: arbitrum,
-    address: chosenToken,
-  });
-  const { data: chosenTokenBalance } = useReadContract({
-    contract: contract,
-    method: balanceOf,
-  });
+const { Option } = Select;
 
-  const [amount, setAmount] = useState(0);
-  const [inputValue, setInputValue] = useState("");
-  const handleInputChange = async (eventValue) => {
-    if (eventValue === "") {
-      return;
-    }
-    setInputValue(eventValue);
-    setAmount(eventValue);
-  };
+const TokenDropdownInput = memo(
+  ({
+    selectedToken,
+    setSelectedToken,
+    investmentAmount,
+    setInvestmentAmount,
+  }) => {
+    const chainId = useActiveWalletChain();
+    const contract = getContract({
+      client: THIRDWEB_CLIENT,
+      chain: arbitrum,
+      address: selectedToken,
+    });
 
-  const handleOnClickMax = async () => {
-    setAmount(chosenTokenBalance.displayValue);
-    setInputValue(chosenTokenBalance.displayValue);
-    handleInputChange(chosenTokenBalance.displayValue);
+    const { data: chosenTokenBalance } = useReadContract({
+      contract: contract,
+      method: balanceOf,
+    });
 
-    // TODO(david): find a better way to implement.
-    // Since `setAmount` need some time to propagate, the `amount` would be 0 at the first click.
-    // then be updated to the correct value at the second click.
-    if (approveAmount < amount || amount === 0) {
-      setApproveReady(false);
-    }
-  };
-  return (
-    <>
-      <Space.Compact
-        style={{
-          margin: "10px 0",
-        }}
-        role="crypto_input"
-      >
-        {selectBefore(
-          (value) => {
-            setChosenToken(value);
-          },
-          "address",
-          chainId?.id,
-        )}
-        <NumericInput
-          placeholder={`Balance: ${
-            chosenTokenBalance ? chosenTokenBalance.displayValue : 0
-          }`}
-          value={inputValue}
-          onChange={(value) => {
-            handleInputChange(value);
-          }}
-        />
-        <Button type="primary" onClick={handleOnClickMax}>
-          Max
-        </Button>
-      </Space.Compact>
-    </>
-  );
-};
+    const handleInputChange = (eventValue) => {
+      setInvestmentAmount(eventValue);
+    };
+
+    const handleOnClickMax = () => {
+      const balance = chosenTokenBalance ? chosenTokenBalance.displayValue : "";
+      setInvestmentAmount(balance);
+      handleInputChange(balance);
+    };
+
+    useEffect(() => {
+      if (selectedToken) {
+        // Fetch balance or any other necessary data when selectedToken changes
+      }
+    }, [selectedToken]);
+
+    return (
+      <>
+        <Space style={{ margin: "10px 0" }} role="crypto_input">
+          {selectBefore(
+            (value) => {
+              setSelectedToken(value);
+            },
+            "address",
+            chainId?.id,
+            selectedToken, // Pass the current selected token
+          )}
+          <NumericInput
+            placeholder={`Balance: ${
+              chosenTokenBalance ? chosenTokenBalance.displayValue : 0
+            }`}
+            value={investmentAmount}
+            onChange={(value) => {
+              handleInputChange(value);
+            }}
+          />
+          <Button type="primary" onClick={handleOnClickMax}>
+            Max
+          </Button>
+        </Space>
+      </>
+    );
+  },
+);
+
 export default TokenDropdownInput;
