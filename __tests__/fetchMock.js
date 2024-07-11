7871a141d44b54eb8789bef5e1729f13a1246d78
davidtnfsh <davidtnfsh@gmail.com>
2024-07-11 13:59:46 +0900

    format

--- __tests__/fetchMock.js
+++ __tests__/fetchMock.js
@@ -1,16 +1,18 @@
-import axios from 'axios'
+import axios from "axios";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 const searchWalletAddress = "undefined";
 const walletAddress = "0xca35a10c9622febfa889410efb9b905b26221c37";
 
 export const fetchApr = async () => {
-    const response = await axios.get(`${API_URL}/bundle_portfolio/${
-          searchWalletAddress === undefined
-            ? walletAddress
-            : searchWalletAddress.toLowerCase().trim().replace("/", "")
-        }?refresh=true`)
-    const { portfolio_apr } = response
-    console.log("portfolio_apr", portfolio_apr)
-    return portfolio_apr
-}
+  const response = await axios.get(
+    `${API_URL}/bundle_portfolio/${
+      searchWalletAddress === undefined
+        ? walletAddress
+        : searchWalletAddress.toLowerCase().trim().replace("/", "")
+    }?refresh=true`,
+  );
+  const { portfolio_apr } = response;
+  console.log("portfolio_apr", portfolio_apr);
+  return portfolio_apr;
+};
