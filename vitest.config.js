f50cd65fa99b4c9c4e587323fbd8679deba85c88
Chris.Fan <fanchi0917@gmail.com>
2024-07-15 14:25:47 +0800

    [chores]: prettier

--- vitest.config.js
+++ vitest.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import { config } from "dotenv";
-import { configDefaults } from 'vitest/config';
+import { configDefaults } from "vitest/config";
 
 export default defineConfig({
   plugins: [react()],
@@ -15,7 +15,7 @@ export default defineConfig({
         "**/out/**",
         "**/.next/**",
         "**/*.config.{js,ts}",
-      ]
+      ],
     },
     env: {
       ...config({ path: "./.env.local" }).parsed,
