1a9c0365d33c7e8342ae020c32bfb5983ca7b23d
Chris.Fan <fanchi0917@gmail.com>
2024-07-15 14:09:54 +0800

    [feat](vitest): add exclude for coverage

--- vitest.config.js
+++ vitest.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import { config } from "dotenv";
+import { configDefaults } from 'vitest/config';
 
 export default defineConfig({
   plugins: [react()],
@@ -9,6 +10,12 @@ export default defineConfig({
     environment: "jsdom",
     coverage: {
       provider: "v8",
+      exclude: [
+        ...configDefaults.exclude,
+        "**/out/**",
+        "**/.next/**",
+        "**/*.config.{js,ts}",
+      ]
     },
     env: {
       ...config({ path: "./.env.local" }).parsed,
