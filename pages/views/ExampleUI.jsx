0d7f0234b2b8acb430b63da017cdfa9f9cd78302
davidtnfsh <davidtnfsh@gmail.com>
2024-08-01 16:53:42 +0900

    wip

--- pages/views/ExampleUI.jsx
+++ pages/views/ExampleUI.jsx
@@ -166,26 +166,6 @@ export default function ExampleUI() {
             offset: 7,
           }}
         >
-          Data updated 1 day ago
-          <button
-            type="button"
-            className="rounded-full bg-indigo-600 p-1 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="size-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
-              />
-            </svg>
-          </button>
           <APRDetails />
           {/* TODO(david): Use this historical chart to track portfolio's APR */}
           {/* {subscriptionStatus ? (
