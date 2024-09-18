683c1d10cf4a60f48877b4a708388170b89cc282
davidtnfsh <davidtnfsh@gmail.com>
2024-09-18 16:39:48 +0700

    [feat]: re-arrange the whole UI

---
+++ pages/profile/index.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { useEffect } from "react";
+import PortfolioMetaTab from "../views/PortfolioMetaTab.jsx";
+import { Row, Col } from "antd";
+import BasePage from "../basePage";
+import styles from "../../styles/Home.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { useActiveAccount } from "thirdweb/react";
+import {
+  fetchDataStart,
+  fetchDataSuccess,
+  fetchDataFailure,
+} from "../../lib/features/apiSlice";
+import { walletAddressChanged } from "../../lib/features/subscriptionSlice";
+import axios from "axios";
+import { useRouter } from "next/router";
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export default function ExampleUI() {
+  const dispatch = useDispatch();
+  const { data } = useSelector((state) => state.api);
+  const account = useActiveAccount();
+  const walletAddress = account?.address.toLocaleLowerCase();
+  const router = useRouter();
+
+  const { query } = router;
+  const searchWalletAddress = query.address;
+
+  useEffect(() => {
+    if (!walletAddress) return;
+    dispatch(walletAddressChanged({ walletAddress: walletAddress }));
+  }, [account]);
+  useEffect(() => {
+    if (!walletAddress && !searchWalletAddress) return;
+    fetchBundlePortfolio(false);
+  }, [searchWalletAddress, walletAddress]);
+  const fetchBundlePortfolio = (refresh) => {
+    dispatch(fetchDataStart());
+    axios
+      .get(
+        `${API_URL}/bundle_portfolio/${
+          searchWalletAddress === undefined
+            ? walletAddress
+            : searchWalletAddress.toLowerCase().trim().replace("/", "")
+        }?refresh=${refresh}`,
+      )
+      .then((response) => response.data)
+      .then((data) => dispatch(fetchDataSuccess(data)))
+      .catch((error) => dispatch(fetchDataFailure(error.toString())));
+  };
+  return (
+    <BasePage>
+      <div className={styles.divInstallment}>
+        <Row
+          gutter={{
+            xs: 8,
+            md: 16,
+          }}
+        >
+          <Col
+            xs={{
+              span: 24,
+              offset: 0,
+            }}
+            md={{
+              span: 18,
+              offset: 3,
+            }}
+          >
+            <PortfolioMetaTab />
+          </Col>
+        </Row>
+      </div>
+    </BasePage>
+  );
+}
--- pages/views/ExampleUI.jsx
+++ pages/views/ExampleUI.jsx
@@ -20,6 +20,7 @@ import { walletAddressChanged } from "../../lib/features/subscriptionSlice";
 import axios from "axios";
 import { Spin } from "antd";
 import { useRouter } from "next/router";
+import Vaults from "../indexes/index.jsx";
 import { timeAgo } from "../../utils/general";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
@@ -42,6 +43,16 @@ export default function ExampleUI() {
   const { strategyMetadata, strategyLoading, error } = useSelector(
     (state) => state.strategyMetadata,
   );
+  const maxAPREntry = Object.entries(strategyMetadata).reduce(
+    (max, [key, strategy]) => {
+      const apr = strategy.portfolioAPR * 100;
+      return apr > max.apr ? { key, apr } : max;
+    },
+    { key: "", apr: -Infinity },
+  );
+
+  const maxAPR = maxAPREntry.apr.toFixed(2);
+  const portfolioName = maxAPREntry.key.replace(" ", "+");
   const router = useRouter();
 
   const { query } = router;
@@ -128,17 +139,15 @@ export default function ExampleUI() {
                   isNaN(strategyMetadata["Stablecoin Vault"]?.portfolioAPR) ? (
                     <Spin />
                   ) : (
-                    Math.max(
-                      ...Object.values(strategyMetadata).map(
-                        (strategy) => strategy.portfolioAPR * 100,
-                      ),
-                    )?.toFixed(2)
+                    maxAPR
                   )}
                   %{" "}
                 </span>
                 APR
               </p>
-              <Link href="/indexes">
+              <Link
+                href={`/indexes/indexOverviews/?portfolioName=${portfolioName}`}
+              >
                 <Button
                   type="primary"
                   className={styles.btnInvest}
@@ -167,61 +176,7 @@ export default function ExampleUI() {
             offset: 3,
           }}
         >
-          <PortfolioMetaTab />
-        </Col>
-        <Col
-          xs={{
-            span: 24,
-            offset: 0,
-          }}
-          md={{
-            span: 18,
-            offset: 3,
-          }}
-        >
-          {data ? (
-            <div className="mt-4 ps-4">
-              <h4 className="text-xl font-semibold text-white">
-                Current Portfolio Status Update
-              </h4>
-              <div className="flex items-center my-1">
-                <span>
-                  Data updated <b>{timeAgo(data.last_updated)}</b> ago
-                </span>
-                <button
-                  type="button"
-                  className="ms-2 rounded-full p-1 text-white shadow-sm"
-                  style={{ backgroundColor: "#5DFDCB" }}
-                  onClick={() => fetchBundlePortfolio(true)}
-                >
-                  <ReloadOutlined className="h-5 w-5 justify-center text-black" />
-                </button>
-              </div>
-              <span className="text-sm text-gray-500">
-                (quota: {data?.user_record_update_times_today.counts}/
-                {data?.user_record_update_times_today.limit})
-              </span>
-            </div>
-          ) : null}
-          <div className="ps-4">
-            <APRDetails />
-          </div>
-          {/* TODO(david): Use this historical chart to track portfolio's APR */}
-          {/* {subscriptionStatus ? (
-            <>
-              <p
-                className="heading-subtitle"
-                style={{
-                  margin: "32px 0",
-                }}
-              >
-                Historical Reward
-              </p>
-              <HistoricalDataChart />
-            </>
-          ) : (
-            <SubscribeWording />
-          )} */}
+          <Vaults />
         </Col>
       </Row>
     </div>
