a3c50fc1503b13ef17075d81ffb25a6230de7450
davidtnfsh <davidtnfsh@gmail.com>
2024-10-24 14:38:02 +0700

    [fixCI]

--- pages/thirdPartyPlugin.jsx
+++ pages/thirdPartyPlugin.jsx
@@ -16,14 +16,20 @@ function ThirdPartyPlugin() {
       {/* End Google Tag Manager */}
 
       {/* Google Tag Manager (noscript) */}
-      <noscript>
-        <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-5X4RZWBX"
-          height="0"
-          width="0"
-          style="display:none;visibility:hidden"
-        ></iframe>
-      </noscript>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `
+            <noscript>
+              <iframe
+                src="https://www.googletagmanager.com/ns.html?id=GTM-5X4RZWBX"
+                height="0"
+                width="0"
+                style="display:none;visibility:hidden"
+              ></iframe>
+            </noscript>
+          `,
+        }}
+      />
       {/* End Google Tag Manager (noscript) */}
 
       {/* Crisp Chat */}
