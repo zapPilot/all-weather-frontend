eb738c2cc8ee31b7b6d008fdbcd518e618f6927f
davidtnfsh <davidtnfsh@gmail.com>
2024-12-09 15:55:44 +0700

    [chores](withdraw): handle small amount issue

--- classes/BasePortfolio.jsx
+++ classes/BasePortfolio.jsx
@@ -374,11 +374,14 @@ export class BasePortfolio {
           actionParams.tokenPricesMappingTable,
         );
         if (protocolUsdBalance === 0) return null;
+        let normalizedZapOutPercentage = actionParams.zapOutPercentage;
+        if (protocolUsdBalance * actionParams.zapOutPercentage < 1) {
+          // to avoid high slippage, we need to zap out at least 1 dollar
+          normalizedZapOutPercentage = 1 / protocolUsdBalance;
+        }
         return protocol.interface.zapOut(
           actionParams.account,
-          protocolUsdBalance * actionParams.zapOutPercentage < 0.1
-            ? 1
-            : Number(actionParams.zapOutPercentage),
+          normalizedZapOutPercentage,
           actionParams.tokenOutAddress,
           actionParams.slippage,
           actionParams.tokenPricesMappingTable,
