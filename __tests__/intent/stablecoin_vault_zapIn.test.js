777e02a8a351db5fc716011b0354f1dceb42243f
davidtnfsh <davidtnfsh@gmail.com>
2025-03-11 11:36:42 +0800

    [test]: add bignumber test case

--- __tests__/intent/stablecoin_vault_zapIn.test.js
+++ __tests__/intent/stablecoin_vault_zapIn.test.js
@@ -1,5 +1,5 @@
 // File: tokenTransfer.test.js
-import { describe, it } from "vitest";
+import { describe, it, expect } from "vitest";
 import { generateIntentTxns } from "../../classes/main.js";
 import { getPortfolioHelper } from "../../utils/thirdwebSmartWallet.ts";
 import { arbitrum } from "thirdweb/chains";
@@ -19,8 +19,77 @@ describe("Stablecoin Vault", () => {
     const userAddress = "0xc774806f9fF5f3d8aaBb6b70d0Ed509e42aFE6F0";
     const tokenSymbol = "usdc";
     const tokenAddress = "0xaf88d065e77c8cc2239327c5edb3a432268e5831";
-    const investmentAmount = 100;
+    const investmentAmount = 100000;
     const tokenDecimals = 6;
+    const zapOutPercentage = NaN;
+    const portfolioHelper = getPortfolioHelper("Stablecoin Vault");
+    await generateIntentTxns(
+      actionName,
+      arbitrum,
+      portfolioHelper,
+      userAddress,
+      tokenSymbol,
+      tokenAddress,
+      investmentAmount,
+      tokenDecimals,
+      zapOutPercentage,
+      setTradingLoss,
+      setStepName,
+      setTotalTradingLoss,
+      setPlatformFee,
+      slippage,
+      rebalancableUsdBalanceDict,
+      userAddress,
+      protocolAssetDustInWallet[
+        arbitrum?.name.toLowerCase().replace(" one", "").replace(" mainnet", "")
+      ],
+      onlyThisChain,
+    );
+  });
+  it("should fail with very big number", async () => {
+    const actionName = "zapIn";
+    const userAddress = "0xc774806f9fF5f3d8aaBb6b70d0Ed509e42aFE6F0";
+    const tokenSymbol = "usdc";
+    const tokenAddress = "0xaf88d065e77c8cc2239327c5edb3a432268e5831";
+    const investmentAmount = 10000000000000;
+    const tokenDecimals = 6;
+    const zapOutPercentage = NaN;
+    const portfolioHelper = getPortfolioHelper("Stablecoin Vault");
+    expect(async () => {
+      await generateIntentTxns(
+        actionName,
+        arbitrum,
+        portfolioHelper,
+        userAddress,
+        tokenSymbol,
+        tokenAddress,
+        investmentAmount,
+        tokenDecimals,
+        zapOutPercentage,
+        setTradingLoss,
+        setStepName,
+        setTotalTradingLoss,
+        setPlatformFee,
+        slippage,
+        rebalancableUsdBalanceDict,
+        userAddress,
+        protocolAssetDustInWallet[
+          arbitrum?.name
+            .toLowerCase()
+            .replace(" one", "")
+            .replace(" mainnet", "")
+        ],
+        onlyThisChain,
+      ).toThrow("number overflow");
+    });
+  });
+  it("should be able to zap-in with Big ETH", async () => {
+    const actionName = "zapIn";
+    const userAddress = "0xc774806f9fF5f3d8aaBb6b70d0Ed509e42aFE6F0";
+    const tokenSymbol = "eth";
+    const tokenAddress = "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1";
+    const investmentAmount = 100;
+    const tokenDecimals = 18;
     const zapOutPercentage = NaN;
     const portfolioHelper = getPortfolioHelper("Stablecoin Vault");
     await generateIntentTxns(
