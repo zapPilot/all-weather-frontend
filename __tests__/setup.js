9a0311e2f7f945166d6d20ef1058a3bb9e4662c8
davidtnfsh <davidtnfsh@gmail.com>
2025-04-13 17:10:05 +0900

    [fixCI]: deprecated convex test + handle mutation error

--- __tests__/setup.js
+++ __tests__/setup.js
@@ -1,25 +1,25 @@
 // Mock MutationObserver
 class MutationObserver {
-    constructor(callback) {
-        this.callback = callback;
-    }
-    observe() {}
-    disconnect() {}
+  constructor(callback) {
+    this.callback = callback;
+  }
+  observe() {}
+  disconnect() {}
 }
 
 global.MutationObserver = MutationObserver;
 
 // Mock window.matchMedia
-Object.defineProperty(window, 'matchMedia', {
-    writable: true,
-    value: jest.fn().mockImplementation(query => ({
-        matches: false,
-        media: query,
-        onchange: null,
-        addListener: jest.fn(),
-        removeListener: jest.fn(),
-        addEventListener: jest.fn(),
-        removeEventListener: jest.fn(),
-        dispatchEvent: jest.fn(),
-    })),
-}); 
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['<rootDir>/__tests__/setup.js'],
-    moduleNameMapper: {
-        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    },
-    transform: {
-        '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
-    },
-}; 
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/__tests__/setup.js"],
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
+  },
+};
