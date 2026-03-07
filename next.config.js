325c7ed628ad4f00d8f144433449d96731cae0be
張泰瑋(Chang Tai Wei) <davidtnfsh@gmail.com>
2026-03-07 11:51:32 +0900

    fix: yarn build

--- next.config.js
+++ next.config.js
@@ -77,8 +77,7 @@ const nextConfig = {
   assetPrefix: "/", // Keep this
 };
 
-// Production plugins - Sentry and PWA
-const { withSentryConfig } = require("@sentry/nextjs");
+// Production plugins - PWA
 const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
@@ -86,36 +85,4 @@ const withPWA = require("next-pwa")({
   disable: isDev, // Disabled in dev for memory
 });
 
-module.exports = withPWA(
-  withSentryConfig(
-    nextConfig,
-    {
-      // For all available options, see:
-      // https://github.com/getsentry/sentry-webpack-plugin#options
-
-      // Suppresses source map uploading logs during build
-      silent: true,
-      org: "all-weather-portfolio",
-      project: "webapp",
-    },
-    {
-      // For all available options, see:
-      // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-      // Upload a larger set of source maps for prettier stack traces (increases build time)
-      widenClientFileUpload: true,
-
-      // Transpiles SDK to be compatible with IE11 (increases bundle size)
-      transpileClientSDK: true,
-
-      // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-      tunnelRoute: "/monitoring",
-
-      // Hides source maps from generated client bundles
-      hideSourceMaps: true,
-
-      // Automatically tree-shake Sentry logger statements to reduce bundle size
-      disableLogger: true,
-    },
-  ),
-);
+module.exports = withPWA(nextConfig);
